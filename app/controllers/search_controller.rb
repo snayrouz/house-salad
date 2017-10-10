2931a97656b3cde3a478d879cc89a0c7519bdf7e
snayrouz <samuel.nayrouz@gmail.com>
2017-10-10 11:11:10 -0600

    Add api consumption method to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.request :url_encoded
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+    conn.headers["X-API-KEY"] = "S9JON3ruNOI6XiyymcnZ7gtsjnToPxuXyT0bgeaX"
+    response = @conn.get("/congress/v1/members/house/CO/current.json")
+    @members = JSON.parse(response.body, symbolize_names:true)[:results]
   end
-
 end

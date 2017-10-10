18a6a91611b44ddf35c16eff2a1869ada19d926d
snayrouz <samuel.nayrouz@gmail.com>
2017-10-10 11:19:01 -0600

    Commit before figaro

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
       faraday.adapter Faraday.default_adapter
     end
     conn.headers["X-API-KEY"] = "S9JON3ruNOI6XiyymcnZ7gtsjnToPxuXyT0bgeaX"
-    response = @conn.get("/congress/v1/members/house/CO/current.json")
+    response = conn.get("/congress/v1/members/house/#{params[:state]}/current.json")
     @members = JSON.parse(response.body, symbolize_names:true)[:results]
   end
 end
